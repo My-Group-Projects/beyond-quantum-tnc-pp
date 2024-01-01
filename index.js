0e3fc19e02a97a77569071fbc034b61550334e95
Danish-Mahajan-16 <77715863+Danish-Mahajan-16@users.noreply.github.com>
2024-01-01 21:50:29 +0530

    Completed JS code for the tab to wwitch between T&C and Privacy Policy

---
+++ index.js
@@ -0,0 +1,26 @@
+function openTab(event, tabName) {
+    let i, tabContent, tabLinks;
+
+    // Remove the "active" class from all elements with the class "tablink"
+    tabLinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].classList.remove("active");
+    }
+
+    tabContent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].classList.remove("active");
+    }
+
+    // Add the "active" class specifically to the clicked tab and its content
+    document.getElementById(tabName).classList.add("active");
+    event.currentTarget.classList.add("active");
+
+    // Remove the "active" class from the "Terms and Conditions" button
+    if (tabName === 'privacy') {
+      document.getElementById('termsButton').classList.remove("active");
+    }
+  }
+
+  //This will simulate a click on the terms button so that the content of the T&C shows
+  document.getElementById('termsButton').click();
